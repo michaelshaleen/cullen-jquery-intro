21b7f24e9562ffd8b9def80a4662f1ec33f1a258
Michael Shaleen <shaleenmichael@protonmail.com>
2021-01-28 14:51:05 -0600

    appending to DOM

--- myAwesomeScript.js
+++ myAwesomeScript.js
@@ -12,8 +12,30 @@ function onReady() {
   $('h2').addClass('froggy');
 
   const h1Text = $('h1').text();
-  console.log('h1Text is', h1Text);
+  console.log('h1Text is', h1Text); // get stuff from the DOM
 
   const louderText = h1Text.toUpperCase() + '!!!!';
   $('h1').text(louderText);
+
+  $('#button').on('click', whenIAddAHarmonica); // not calling function, referring
+  //calling would be whenIAddAHarmonica();
+  $('.deleteBut').on('click', deleteMe);
+  $(document).on('click', '.deleteBut', deleteMe);
+}
+function deleteMe() {
+  console.log('delete');
+}
+
+function whenIAddAHarmonica() {
+  //TODO
+  console.log('button');
+  //grab data to pass through
+  let brand = $('#brand').val(); // put the input value as the var, grab via JQ on id
+  console.log('brand is ', brand); // console log the val
+  $('#harmonicas').append(` 
+  <li> 
+    Brand is: ${brand}
+    <button class= "deleteBut">Delete Me</button>
+  </li>`); // grab id of harmonicas, append so onto DOM, this we want to create a list item then append onto that
+  //append brand is and grab brand with jq
 }
