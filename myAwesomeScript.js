fd40b94184afbea2a56d1782ee8f7758a8828e0e
Michael Shaleen <shaleenmichael@protonmail.com>
2021-01-28 13:55:42 -0600

    more jq styling

--- myAwesomeScript.js
+++ myAwesomeScript.js
@@ -7,5 +7,12 @@ function onReady() {
   const h1 = $('h1');
   console.log('h1', h1);
 
-  const h2 = $();
+  const h2 = $('h2');
+  //h2.css('background-color', 'blue');
+  $('h2').addClass('froggy');
+  const h1Text = $('h1').text();
+  console.log('h1Text is', h1Text);
+
+  const louderText = h1Text.toUpperCase() + '!!!!';
+  $('h1').text(louderText);
 }
